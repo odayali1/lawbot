b2a57a511dd193c7aead2fa01d1d2156735cea8f
LawBot Developer <dollanyapp@gmail.com>
2025-08-10 19:10:41 +0300

    Update ESLint to v9.15.0 and resolve deprecation warnings: migrate to flat config format, add @eslint/js and globals packages, remove deprecated eslint-config-standard

---
+++ eslint.config.js
@@ -0,0 +1,38 @@
+import js from '@eslint/js';
+import globals from 'globals';
+
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'script',
+      globals: {
+        ...globals.node,
+        ...globals.es2021
+      }
+    },
+    rules: {
+      'no-console': 'off',
+      'no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_'
+        }
+      ],
+      'indent': ['error', 2],
+      'quotes': ['error', 'single'],
+      'semi': ['error', 'never'],
+      'comma-dangle': ['error', 'never'],
+      'space-before-function-paren': ['error', 'always']
+    }
+  },
+  {
+    files: ['**/*.test.js', '**/*.spec.js'],
+    languageOptions: {
+      globals: {
+        ...globals.jest
+      }
+    }
+  }
+];
